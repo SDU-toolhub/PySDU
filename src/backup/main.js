d75f6fa7076e6a63f7ab9b02e8962f2859ace88b
Vincent Son <107471539+szw0407@users.noreply.github.com>
2023-12-06 20:33:42 +0800

    Delete backup files and unused code

--- src/backup/main.js
+++
@@ -1,9 +0,0 @@
-import Fingerprint2 from 'fingerprintjs2';
-
-require('./fingerprint2')
-
-function x64hash128(s, seed) {
-    return Fingerprint2.x64hash128(s, seed);
-}
-
-console.log(x64hash128('123', 31));
